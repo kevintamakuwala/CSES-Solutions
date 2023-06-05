084a59cab93e7d4e59c08e23de9187c7366fc7b6
kevintamakuwala <kevintamakuwala16@gmail.com>
2023-06-06 03:21:34 +0530

    DP: 1638

--- Dp/1638.cpp
+++ Dp/1638.cpp
@@ -2,19 +2,48 @@
 using namespace std;
 typedef long long int ll;
 #define rep(i, s, n) for (int i = s; i < n; i++)
-typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef vector<vll> vvll;
 
 int main()
 {
-    ll n, k;
-    cin >> n >> k;
-    ll a[n];
+    ll n;
+    cin >> n;
+    vector<string> v;
     rep(i, 0, n)
     {
-        cin >> a[i];
+        string s;
+        cin >> s;
+        v.push_back(s);
     }
-    
+    if (v[0][0] == '*' or v[n - 1][n - 1] == '*')
+    {
+        cout << "0\n";
+        return 0;
+    }
+    vll prev(n + 1, 0), cur = prev;
+    rep(i, 0, n)
+    {
+        rep(j, 0, n)
+        {
+            if (i == 0 and j == 0)
+            {
+                cur[j] = 1;
+                continue;
+            }
+            ll up = 0, left = 0;
+            if (i > 0 and v[i - 1][j] != '*')
+            {
+                up = prev[j];
+            }
+            if (j > 0 and v[i][j - 1] != '*')
+            {
+                left = cur[j - 1];
+            }
+            cur[j] = (up + left) % 1000000007;
+        }
+        prev = cur;
+    }
+    cout << prev[n - 1] << '\n';
     return 0;
 }
